f48985225fbe1a315511811420b723f6aabb57e4
elgogle <elgogle@gmail.com>
2018-01-20 21:13:54 +0800

    增加微信通知

--- damatuCode/damatuWeb.py
+++ damatuCode/damatuWeb.py
@@ -18,8 +18,8 @@ def md5(byte):  # md5加密byte
 
 
 class DamatuApi():
-    ID = '40838'
-    KEY = 'ca9507e17e8d5ddf7c57cd18d8d33010'
+    ID = '53341'
+    KEY = 'a6ac38eb69bdf637dbdbbd865c624181'
     HOST = 'http://api.dama2.com:7766/app/'
 
     def __init__(self, username, password, file_path):
